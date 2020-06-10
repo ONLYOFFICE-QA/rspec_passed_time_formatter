c661dc604767ac93dd29dbadfe2ca8b1b459184b
Pavel Lobashov <shockwavenn@gmail.com>
2020-06-10 13:59:25 +0300

    Fix rubocop `RSpec/ExampleWording` cop

--- spec/rspec_passed_time_formatter_spec.rb
+++ spec/rspec_passed_time_formatter_spec.rb
@@ -7,12 +7,12 @@ RSpec.describe RspecPassedTimeFormatter do
     expect(RspecPassedTimeFormatter::VERSION).not_to be nil
   end
 
-  it 'should return just seconds for sub 60 seconds' do
+  it 'returns just seconds for sub 60 seconds' do
     run_spec_with_formatter("#{Dir.pwd}/spec/simple_spec.rb")
     expect(File.read('out.html')).to include('Test finished at')
   end
 
-  it 'should show time when started' do
+  it 'shows time when started' do
     run_spec_with_formatter("#{Dir.pwd}/spec/simple_spec.rb")
     expect(File.read('out.html')).to include('Test started at:')
   end
