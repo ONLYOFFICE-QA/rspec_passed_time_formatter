05aca0265270232fe74bd3ec0a14d5ec9ac4e187
Pavel Lobashov <shockwavenn@gmail.com>
2022-03-01 18:36:42 +0300

    Changes from `rubocop-rspec` update to 2.9.0

--- spec/rspec_passed_time_formatter_spec.rb
+++ spec/rspec_passed_time_formatter_spec.rb
@@ -4,7 +4,7 @@ require 'spec_helper'
 
 RSpec.describe RspecPassedTimeFormatter do
   it 'has a version number' do
-    expect(RspecPassedTimeFormatter::VERSION).not_to be nil
+    expect(RspecPassedTimeFormatter::VERSION).not_to be_nil
   end
 
   it 'returns just seconds for sub 60 seconds' do
